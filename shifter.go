7c32106e96c8cbaebe0711bf0beff9391a7d6936
Lincoln Stoll <lincoln.stoll@gmail.com>
2016-07-31 01:44:39 -0400

    caching, pre-stream etc.

--- shifter.go
+++ shifter.go
@@ -1,7 +1,9 @@
 package tjts
 
 import (
+	"encoding/gob"
 	"log"
+	"os"
 	"time"
 )
 
@@ -20,10 +22,10 @@ type memShifter struct {
 
 // memStore is the in-memory structure for a stream
 type streamStore struct {
-	timeStore  []time.Time
-	chunkStore [][]byte
-	currPos    int
-	len        int
+	TimeStore  []time.Time
+	ChunkStore [][]byte
+	CurrPos    int
+	Len        int
 }
 
 // subscriber represents a consumer and their "index"
@@ -34,39 +36,61 @@ type subscriber struct {
 }
 
 // NewMemShifter returns a shifter for the given stream that uses memory  as
-// it's storage mechanism. It will buffer for maxOffset
-func NewMemShifter(data chan []byte, timePerChunk time.Duration, maxOffset time.Duration) Shifter {
+// it's storage mechanism. It will buffer for maxOffset. If cacheFile isn't
+// empty, the contents of the memory store will be written here every
+// cacheInterval. If this file exists and start time, it will be read in to
+// memory
+func NewMemShifter(data chan []byte, timePerChunk time.Duration, maxOffset time.Duration, cacheFile string, cacheInterval time.Duration) Shifter {
 	storeSize := int(maxOffset / timePerChunk)
-	s := &memShifter{
+	m := &memShifter{
 		dataIn: data,
 		store: &streamStore{
-			timeStore:  make([]time.Time, storeSize),
-			chunkStore: make([][]byte, storeSize),
-			currPos:    0,
-			len:        storeSize,
+			TimeStore:  make([]time.Time, storeSize),
+			ChunkStore: make([][]byte, storeSize),
+			CurrPos:    0,
+			Len:        storeSize,
 		},
 	}
-	go s.start()
-	return s
+	if cacheFile != "" {
+		// Read it if it exists
+		f, err := os.Open(cacheFile)
+		if err == nil {
+			log.Printf("Loading cache file from: %q", cacheFile)
+			decoder := gob.NewDecoder(f)
+			err = decoder.Decode(m.store)
+			if err != nil {
+				log.Printf("Error decoding cache file, ignoring: %q", err)
+			}
+		}
+		f.Close()
+
+		go m.cacher(cacheFile, cacheInterval)
+	}
+	go m.start()
+	return m
 }
 
 func (m *memShifter) StreamFrom(offset time.Duration) (chan []byte, chan struct{}) {
-	dataChan := make(chan []byte)
+	dataChan := make(chan []byte, 32)
 	closeChan := make(chan struct{})
 
 	f := false
-	sf := m.store.currPos // Default to start time being "curr pos"
+	sf := m.store.CurrPos // Default to start time being "curr pos"
 	st := time.Now().Add(-offset)
-	for i := m.store.currPos + 1; i < m.store.len; i++ {
-		if m.store.timeStore[i].After(st) {
+	for i := m.store.CurrPos + 1; i < m.store.Len; i++ {
+		sp := i
+		if sp >= m.store.Len {
+			sp = sp - m.store.Len
+		}
+		if m.store.TimeStore[i].After(st) {
 			sf = i
 			f = true
 			break
 		}
 	}
 	if !f {
-		for i := 0; i < m.store.currPos+1; i++ {
-			if m.store.timeStore[i].After(st) {
+		for i := 0; i < m.store.CurrPos+1; i++ {
+			if m.store.TimeStore[i].After(st) {
 				sf = i
 				break
 			}
@@ -75,6 +99,20 @@ func (m *memShifter) StreamFrom(offset time.Duration) (chan []byte, chan struct{
 	log.Printf("starting at %d", sf)
 	sub := &subscriber{data: dataChan, currPos: sf}
 	m.subscribers = append(m.subscribers, sub)
+
+	// Pre seed chunks to give the user a "cache"
+	for i := sf - 5; i < sf; i++ {
+		sp := i
+		if sp < 0 {
+			sp = m.store.Len + sp
+		}
+		if len(m.store.ChunkStore[sp]) == 0 {
+			// Nothing here yet, bail out
+			break
+		}
+		log.Printf("pre-seeding chunk from %d", sp)
+		dataChan <- m.store.ChunkStore[sp]
+	}
 
 	go func() {
 		select {
@@ -92,11 +130,11 @@ func (m *memShifter) start() {
 
 	for d := range m.dataIn {
 		// Store the chunks, update the offset
-		m.store.chunkStore[m.store.currPos] = d
-		m.store.timeStore[m.store.currPos] = time.Now()
-		m.store.currPos++
-		if m.store.currPos >= m.store.len {
-			m.store.currPos = 0
+		m.store.ChunkStore[m.store.CurrPos] = d
+		m.store.TimeStore[m.store.CurrPos] = time.Now()
+		m.store.CurrPos++
+		if m.store.CurrPos >= m.store.Len {
+			m.store.CurrPos = 0
 		}
 
 		// For every subscriber channel, send them their current chunk
@@ -106,16 +144,36 @@ func (m *memShifter) start() {
 				continue
 			}
 			select {
-			case sub.data <- m.store.chunkStore[sub.currPos]:
+			case sub.data <- m.store.ChunkStore[sub.currPos]:
+				// log.Printf("Send chunk len %d from pos %d to sub\n", len(m.store.ChunkStore[sub.currPos]), sub.currPos)
 			default:
 				log.Printf("Err sending data to subscribed %#v", sub)
 				close(sub.data)
 			}
 			sub.currPos++
-			if sub.currPos >= m.store.len {
+			if sub.currPos >= m.store.Len {
 				sub.currPos = 0
 			}
 		}
 	}
 	log.Print("Offset store ending. This is an error!!!!!")
+}
+
+func (m *memShifter) cacher(path string, interval time.Duration) {
+	// Start the cacher
+	for range time.NewTicker(interval).C {
+		log.Printf("Writing stream cache to %q", path)
+		f, err := os.Create(path + ".tmp")
+		if err != nil {
+			log.Printf("Error creating cache file: %q", err)
+			continue
+		}
+		encoder := gob.NewEncoder(f)
+		encoder.Encode(m.store)
+		f.Close()
+		if err := os.Rename(path+".tmp", path); err != nil {
+			log.Printf("Error renaming temp cache file: %q", err)
+
+		}
+	}
 }
