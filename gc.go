d4bc74cf5218e7736fe62af5513b80f13a0aee2d
Lincoln Stoll <me@lds.li>
2021-09-17 14:13:29 -0500

    better gc logging and error reporting
    seeing weirdness in prod, so troubleshoot

--- gc.go
+++ gc.go
@@ -15,6 +15,9 @@ const (
 	sessMaxAge = 12 * time.Hour
 	// age at which we purge a chunk
 	chunkMaxAge = 12 * time.Hour
+
+	// max number of expired chunks we act on at once
+	expiredChunksMax = 1000
 )
 
 // garbageCollector periodically cleans up old data
@@ -64,60 +67,104 @@ func (g *garbageCollector) Interrupt(_ error) {
 
 func (g *garbageCollector) collect() error {
 	ctx := context.Background()
-	now := time.Now()
 
-	g.l.Debug("GC sessions")
+	if err := g.gcSessions(ctx); err != nil {
+		return err
+	}
 
-	_, err := g.db.ExecContext(ctx,
+	ecs, err := g.expiredChunks(ctx)
+	if err != nil {
+		return err
+	}
+	if len(ecs) < 1 {
+		return nil
+	}
+
+	if err := g.gcChunks(ctx, ecs); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (g *garbageCollector) gcSessions(ctx context.Context) error {
+	g.l.Debug("start gc sessions")
+	res, err := g.db.ExecContext(ctx,
 		`delete from sessions where updated_at < $1`,
-		now.Add(-sessMaxAge).UTC())
+		time.Now().Add(-sessMaxAge).UTC())
 	if err != nil {
 		return fmt.Errorf("gc sessions: %v", err)
 	}
+	ri, err := res.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("getting session delete rows affected: %d", err)
+	}
+	g.l.Debugf("gc'd %d sessions", ri)
+	return nil
+}
 
-	g.l.Debug("GC chunks")
+type delRow struct {
+	ID       int
+	StreamID string
+	ChunkID  string
+}
+
+func (g *garbageCollector) expiredChunks(ctx context.Context) ([]delRow, error) {
+	g.l.Debug("check expired chunks")
 
 	rows, err := g.db.QueryContext(ctx,
-		`select id, stream_id, chunk_id from chunks where fetched_at < $1`,
-		now.Add(-chunkMaxAge).UTC(),
+		`select id, stream_id, chunk_id from chunks where fetched_at < $1 limit $2`,
+		time.Now().Add(-chunkMaxAge).UTC(), expiredChunksMax,
 	)
 	if err != nil {
-		return fmt.Errorf("fetching expired chunks: %v", err)
+		return nil, fmt.Errorf("fetching expired chunks: %v", err)
 	}
 	defer rows.Close()
 
-	type delRow struct {
-		ID       int
-		StreamID string
-		ChunkID  string
-	}
 	var delrows []delRow
-
-	// TODO - consider if we might need batching here
 
 	for rows.Next() {
 		dr := delRow{}
 
 		if err := rows.Scan(&dr.ID, &dr.StreamID, &dr.ChunkID); err != nil {
-			return fmt.Errorf("scanning row: %v", err)
+			return nil, fmt.Errorf("scanning row: %v", err)
 		}
 
 		delrows = append(delrows, dr)
 	}
 
+	if err := rows.Close(); err != nil {
+		return nil, fmt.Errorf("closing rows: %v", err)
+	}
+
+	g.l.Debugf("found %d expired chunks (max %d)", len(delrows), expiredChunksMax)
+
+	return delrows, nil
+}
+func (g *garbageCollector) gcChunks(ctx context.Context, delrows []delRow) error {
+	g.l.Debug("start gc chunks")
+
 	for _, dr := range delrows {
 		if err := execTx(ctx, g.db, func(ctx context.Context, tx *sql.Tx) error {
-			if _, err := tx.ExecContext(ctx, `delete from chunks where id = $1`, dr.ID); err != nil {
+			res, err := tx.ExecContext(ctx, `delete from chunks where id = $1`, dr.ID)
+			if err != nil {
 				return err
 			}
 			if err := g.dcs.DeleteChunk(ctx, dr.StreamID, dr.ChunkID); err != nil {
 				return fmt.Errorf("deleting chunk %s/%s: %v", dr.StreamID, dr.ChunkID, err)
 			}
+			ri, err := res.RowsAffected()
+			if err != nil {
+				return fmt.Errorf("getting delete chunk rows affected: %d", err)
+			}
+			g.l.Debugf("deleting chunk %s/%s: %d rows affected", dr.StreamID, dr.ChunkID, ri)
 			return nil
 		}); err != nil {
 			return err
 		}
 	}
+
+	g.l.Debug("end gc chunks")
 
 	return nil
 }
